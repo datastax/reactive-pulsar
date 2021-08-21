b5ba3712af0a919358741ac9c3f3f59e477513d0
Lari Hotari <lari@hotari.net>
2021-08-21 16:51:40 +0300

    Add logging to message handler when there's an unexpected error or the handler completes unexpectedly

--- reactive-pulsar-adapter/src/main/java/com/github/lhotari/reactive/pulsar/adapter/DefaultReactiveMessageHandler.java
+++ reactive-pulsar-adapter/src/main/java/com/github/lhotari/reactive/pulsar/adapter/DefaultReactiveMessageHandler.java
@@ -1,10 +1,13 @@
 package com.github.lhotari.reactive.pulsar.adapter;
 
 import java.util.concurrent.atomic.AtomicReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.Disposable;
 import reactor.core.publisher.Mono;
 
 class DefaultReactiveMessageHandler implements ReactiveMessageHandler {
+    private final Logger LOG = LoggerFactory.getLogger(DefaultReactiveMessageHandler.class);
     private final AtomicReference<Disposable> killSwitch = new AtomicReference<>();
     private final Mono<Void> pipeline;
 
@@ -17,12 +20,22 @@ class DefaultReactiveMessageHandler implements ReactiveMessageHandler {
         if (killSwitch.get() != null) {
             throw new IllegalStateException("Message handler is already running.");
         }
-        Disposable disposable = pipeline.subscribe();
+        Disposable disposable = pipeline.subscribe(null, this::logError, this::logUnexpectedCompletion);
         if (!killSwitch.compareAndSet(null, disposable)) {
             disposable.dispose();
             throw new IllegalStateException("Message handler was already running.");
         }
         return this;
+    }
+
+    private void logError(Throwable throwable) {
+        LOG.error("ReactiveMessageHandler was unexpectedly terminated.", throwable);
+    }
+
+    private void logUnexpectedCompletion() {
+        if (isRunning()) {
+            LOG.error("ReactiveMessageHandler was unexpectedly completed.");
+        }
     }
 
     @Override
