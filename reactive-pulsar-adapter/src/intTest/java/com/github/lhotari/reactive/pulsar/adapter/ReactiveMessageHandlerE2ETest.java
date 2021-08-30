055fbc9e67c007e60e88c9c98951145a03b080de
Lari Hotari <lari@hotari.net>
2021-08-30 14:31:05 +0300

    Add end-to-end test for verifying message ordering and parallel processing

--- reactive-pulsar-adapter/src/intTest/java/com/github/lhotari/reactive/pulsar/adapter/ReactiveMessageHandlerE2ETest.java
+++ reactive-pulsar-adapter/src/intTest/java/com/github/lhotari/reactive/pulsar/adapter/ReactiveMessageHandlerE2ETest.java
@@ -2,19 +2,26 @@ package com.github.lhotari.reactive.pulsar.adapter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
+import java.nio.ByteBuffer;
+import java.time.Duration;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.apache.pulsar.client.api.PulsarClient;
 import org.apache.pulsar.client.api.Schema;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
 public class ReactiveMessageHandlerE2ETest {
+
+    public static final int KEYS_COUNT = 100;
+    public static final int ITEMS_PER_KEY_COUNT = 100;
 
     @Test
     void shouldConsumeMessages() throws Exception {
@@ -57,5 +64,108 @@ public class ReactiveMessageHandlerE2ETest {
                 assertThat(messages).isEqualTo(Flux.range(1, 100).map(Object::toString).collectList().block());
             }
         }
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = { false, true })
+    void shouldRetainMessageOrder(boolean parallelize) throws Exception {
+        try (PulsarClient pulsarClient = SingletonPulsarContainer.createPulsarClient()) {
+            String topicName = "test" + UUID.randomUUID();
+            // create subscription to retain messages
+            pulsarClient.newConsumer(Schema.INT32).topic(topicName).subscriptionName("sub").subscribe().close();
+
+            ReactivePulsarClient reactivePulsarClient = ReactivePulsarClient.create(pulsarClient);
+
+            ReactiveMessageSender<Integer> messageSender = reactivePulsarClient
+                .messageSender(Schema.INT32)
+                .topic(topicName)
+                .build();
+
+            List<MessageSpec<Integer>> messageSpecs = generateRandomOrderedMessagesWhereSingleKeyIsOrdered();
+
+            messageSender.sendMessages(Flux.fromIterable(messageSpecs)).blockLast();
+
+            ConcurrentMap<Integer, List<Integer>> messages = new ConcurrentHashMap<>();
+            CountDownLatch latch = new CountDownLatch(messageSpecs.size());
+
+            List<Integer> orderedSequence = IntStream
+                .rangeClosed(1, ITEMS_PER_KEY_COUNT)
+                .boxed()
+                .collect(Collectors.toList());
+
+            ReactiveMessageHandlerBuilder.OneByOneMessageHandlerBuilder<Integer> reactiveMessageHandlerBuilder = ReactiveMessageHandlerBuilder
+                .builder(
+                    reactivePulsarClient
+                        .messageConsumer(Schema.INT32)
+                        .consumerConfigurer(consumerBuilder -> consumerBuilder.subscriptionName("sub").topic(topicName))
+                        .build()
+                )
+                .messageHandler(message -> {
+                    Mono<Void> messageHandler = Mono.fromRunnable(() -> {
+                        Integer keyId = Integer.parseInt(message.getProperty("keyId"));
+                        messages.compute(
+                            keyId,
+                            (k, list) -> {
+                                if (list == null) {
+                                    list = new ArrayList<>();
+                                }
+                                list.add(message.getValue());
+                                return list;
+                            }
+                        );
+                        latch.countDown();
+                    });
+                    if (parallelize) {
+                        // add 10 millisecond delay which would lead to the execution timeout unless
+                        // messages are handled in parallel
+                        messageHandler = Mono.delay(Duration.ofMillis(10)).then(messageHandler);
+                    }
+                    return messageHandler;
+                });
+            if (parallelize) {
+                reactiveMessageHandlerBuilder.concurrent().concurrency(KEYS_COUNT).keyOrdered(true);
+            }
+            try (ReactiveMessageHandler reactiveMessageHandler = reactiveMessageHandlerBuilder.build().start()) {
+                boolean latchCompleted = latch.await(5, TimeUnit.SECONDS);
+                assertThat(latchCompleted).as("processing of all messages should have completed").isTrue();
+                for (int i = 1; i <= KEYS_COUNT; i++) {
+                    assertThat(messages.get(i)).as("keyId %d", i).containsExactlyElementsOf(orderedSequence);
+                }
+            }
+        }
+    }
+
+    private List<MessageSpec<Integer>> generateRandomOrderedMessagesWhereSingleKeyIsOrdered() {
+        List<Queue<MessageSpec<Integer>>> remainingMessages = Flux
+            .range(1, KEYS_COUNT)
+            .concatMap(keyId -> {
+                byte[] keyBytes = ByteBuffer.allocate(4).putInt(keyId).array();
+                return Flux
+                    .range(1, ITEMS_PER_KEY_COUNT)
+                    .map(i ->
+                        Tuples.of(
+                            keyId,
+                            MessageSpec.builder(i).orderingKey(keyBytes).property("keyId", keyId.toString()).build()
+                        )
+                    );
+            })
+            .collectMultimap(Tuple2::getT1, Tuple2::getT2)
+            .map(Map::values)
+            .block()
+            .stream()
+            .map(LinkedBlockingQueue::new)
+            .collect(Collectors.toList());
+
+        List<MessageSpec<Integer>> messageSpecs = new ArrayList<>(KEYS_COUNT * ITEMS_PER_KEY_COUNT);
+        while (messageSpecs.size() < KEYS_COUNT * ITEMS_PER_KEY_COUNT) {
+            int randomIndex = ThreadLocalRandom.current().nextInt(remainingMessages.size());
+            Queue<MessageSpec<Integer>> specsForKey = remainingMessages.get(randomIndex);
+            MessageSpec<Integer> messageSpec = specsForKey.poll();
+            messageSpecs.add(messageSpec);
+            if (specsForKey.size() == 0) {
+                remainingMessages.remove(randomIndex);
+            }
+        }
+        return messageSpecs;
     }
 }
