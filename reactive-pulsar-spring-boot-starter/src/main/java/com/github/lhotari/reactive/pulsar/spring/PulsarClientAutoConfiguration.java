7a8c9ea5652bd33f5ac2214fbf279e3a834fc351
Eddú Meléndez <eddu.melendez@gmail.com>
2022-11-24 14:41:48 -0500

    Move to new auto-configuration
    This change allows applications to be compatible with 2.7.x and 3.x.

--- reactive-pulsar-spring-boot-starter/src/main/java/com/github/lhotari/reactive/pulsar/spring/PulsarClientAutoConfiguration.java
+++ reactive-pulsar-spring-boot-starter/src/main/java/com/github/lhotari/reactive/pulsar/spring/PulsarClientAutoConfiguration.java
@@ -4,13 +4,13 @@ import org.apache.pulsar.client.api.PulsarClient;
 import org.apache.pulsar.client.api.PulsarClientException;
 import org.apache.pulsar.client.impl.ClientBuilderImpl;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 
-@Configuration(proxyBeanMethods = false)
+@AutoConfiguration
 @EnableConfigurationProperties(PulsarClientConfig.class)
 public class PulsarClientAutoConfiguration {
 
